844f6b3f9065c1f18dce8ebc16eb644a7ec4a283
Vasu Thakral <vasuthakral18@gmail.com>
2025-10-29 18:58:08 +0530

    useEffect hook,Master List,Search-Bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,16 @@
 import RestrauntCard from "./RestrauntCard";
-import MockData from "../utils/MockData";
+import MockData from "../utils/MockData"; //Now using shimmer ui
+import Shimmer from "./Shimmer";
 
 import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [list_of_restraunt, set_list_of_restraunt] = useState(MockData);
+  const [list_of_restraunt, set_list_of_restraunt] = useState([]); //Emty array to prevent error->Can't acces property of Undefined->Master List
+
+  const [searchText, setSearchText] = useState("");
+  const [filterList, setFilterList] = useState([]);
+
+  console.log(filterList); //The entire component is getting re-render and console.log() is in component
 
   useEffect(() => {
     fxn();
@@ -15,29 +21,67 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.769223219109158&lng=76.57429534004214&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
-    const fetched = await fetch_data.json(fetch_data);
+    const fetched = await fetch_data.json();
+
+    //Set both master and slave list
     set_list_of_restraunt(
       fetched?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
+
+    setFilterList(
+      fetched?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
+
+  if (list_of_restraunt.length === 0) {
+    return <Shimmer />;
+  }
+
   return (
     <div className="bodyContainer">
+      <div className="search">
+        <input
+          type="text"
+          className="search-box"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(
+              e.target.value
+            ); /* Om every key press entire component is getting re-rendering */
+          }}
+        />
+        <button
+          onClick={() => {
+            //Filter the restraunt card and updates Ui->Bind the value 0f state variable with input box
+            const filter_res = list_of_restraunt.filter((ele) => {
+              return ele.info.name
+                .toLowerCase()
+                .includes(searchText.toLowerCase()); //Print the data for path
+            });
+
+            setFilterList(filter_res);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <button
         className="filter-btn"
         onClick={() => {
           const filtered_list_of_restraunt = list_of_restraunt.filter((ele) => {
             return ele.info.avgRating >= 4.5;
           });
-          set_list_of_restraunt(filtered_list_of_restraunt);
+          setFilterList(filtered_list_of_restraunt);
         }}
       >
         Top rated Restraunts
       </button>
 
       <div className="resContainer">
-        {list_of_restraunt.map((ele) => {
-          return <RestrauntCard key={ele.info.id} resData={ele} />;
+        {filterList.map((ele) => {
+          return <RestrauntCard key={ele?.info?.id} resData={ele} />;
         })}
       </div>
     </div>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,29 @@
+const Shimmer = () => {
+  return (
+    <div className="wrapper">
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+      <div className="shimmer">Cards</div>
+    </div>
+  );
+};
+
+export default Shimmer;
